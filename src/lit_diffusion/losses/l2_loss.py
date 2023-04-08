537175d1103695c27e0d8ba56708be2fa2d52fe6
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-04-08 16:42:58 +0200

    debug

--- src/lit_diffusion/losses/l2_loss.py
+++ src/lit_diffusion/losses/l2_loss.py
@@ -10,6 +10,9 @@ class L2Loss(nn.Module):
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         print("L2 Loss input:", input)
+        print("L2 Loss input shape:", input.shape)
+
         print("L2 Loss target:", target)
+        print("L2 Loss target shape:", target.shape)
         unreduced_loss = F.mse_loss(input=input, target=target, reduction="none")
         return unreduced_loss.mean(list(range(1, len(input.shape))))
