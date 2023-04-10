e108e273f52a036408ec8e794a6bcbf2744ffc13
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-04-10 14:26:45 +0200

    debug

--- src/lit_diffusion/ddpm/lit_ddpm.py
+++ src/lit_diffusion/ddpm/lit_ddpm.py
@@ -135,8 +135,8 @@ class LitDDPM(pl.LightningModule):
 
         # Log any additional metrics
         for metric_name, metric_function in self.training_metrics.items():
-            print("model_x", model_x)
-            print("target", target)
+            print("model_x", model_x.shape)
+            print("target", target.shape)
             self.log(
                 name=metric_name,
                 value=metric_function(model_x, target),
