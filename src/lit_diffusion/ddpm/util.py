44f7f6a773675eb2b255f21d9359018e1ab29201
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-06-17 15:23:18 +0200

    Unified extract into tensor method

--- src/lit_diffusion/ddpm/util.py
+++ src/lit_diffusion/ddpm/util.py
@@ -13,10 +13,24 @@ def default(val, d):
     return d() if isfunction(d) else d
 
 
-def extract_into_tensor(a, t, x_shape):
-    b, *_ = t.shape
-    out = a.gather(-1, t)
-    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
+# def extract_into_tensor(a, t, x_shape):
+#     b, *_ = t.shape
+#     out = a.gather(-1, t)
+#     return out.reshape(b, *((1,) * (len(x_shape) - 1)))
+def extract_into_tensor(arr, timesteps, broadcast_shape):
+    """
+    Extract values from a 1-D numpy array for a batch of indices.
+
+    :param arr: the 1-D numpy array.
+    :param timesteps: a tensor of indices into the array to extract.
+    :param broadcast_shape: a larger shape of K dimensions with the batch
+                            dimension equal to the length of timesteps.
+    :return: a tensor of shape [batch_size, 1, ...] where the shape has K dims.
+    """
+    res = arr[timesteps].float()
+    while len(res.shape) < len(broadcast_shape):
+        res = res[..., None]
+    return res.expand(broadcast_shape)
 
 
 def noise_like(shape, device, repeat=False):
